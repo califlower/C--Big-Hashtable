572c72854db2451d9a351adaa6445d2fbfd8f313
pwndrone <cal13199@gmail.com>
2015-02-24 20:04:39 -0500

    UGHGGHGHGHGH
    Sort of going to inneficient hack together the resize button. FUCK IT

--- SecondProject.c
+++ SecondProject.c
@@ -1,40 +1,87 @@
 #include "stdio.h"
 #include "stdlib.h"
-
-#define TRUE 1
-#define FALSE !TRUE
-#define SIZE 1000
+#define INITIALSIZE 2
+#define LOADFACTOR.5
 
 
 typedef struct node
 {
-    long long value;
+    unsigned long int value;
     struct node *next;
 }hash;
 
-static hash* hashTable[SIZE];
 
 
-long long hashNum(long long value)
+/*the current size of the table*/
+unsigned long int SIZE=INITIALSIZE;
+
+/*A hashtable of pointers to nodes called hash*/
+static hash* table[INITIALSIZE];
+
+/*the counter is used for determining load factor*/
+static unsigned unsigned long int counter=0;
+
+
+
+
+
+
+
+
+/*the hash function that returns an index*/
+unsigned long int hashNum(unsigned long int value)
 {
-    return (value%SIZE);
+	/*Knuth's variant*/
+	/*fck modulus*/
+	unsigned long int tempHashValue=(value*(value+3))%(unsigned long int)SIZE;
+    return tempHashValue;
+
 }
-
-void chainedHashInsert(long long value)
+/*
+void freeOldTable(hash *hashTable[])
 {
-    long long tempValue = hashNum(value);
+	int tempSize=SIZE;
+	unsigned long int x;
+	for(x=0; x<tempSize; x++)
+	{			
+		hash *itr=hashTable[x]; 
+		while (itr!=NULL) 
+		{
+			hash *tmp = itr->next;
+			free(itr);
+			itr = tmp;
+		} 
+	}
 	
-    if(hashTable[tempValue] == NULL)
-	{                              
+}*/
+
+/*Pass the value to be inserted and the name of the hashtable*/
+void chainedHashInsert(unsigned long int value, hash *hashTable[])
+{
+	/* tempValue is the hashed number to be inserted into the table*/
+   unsigned long int tempValue = hashNum(value);
+	
+	
+	/*if the location that we want to insert the value is empty then we do this*/
+    
+	if(hashTable[tempValue] == NULL)
+	{
         hashTable[tempValue] = malloc(sizeof(hash));    
         hashTable[tempValue]->value = value;
         hashTable[tempValue]->next = NULL;
+		return;
     }
+	
+	/*Otherwise we do this here*/
 	else
-	{                                                          
+	{
+		
+		/*create a pointer to a node and set it to the pointer at hashTable[tempValue]*/
         hash *hashNode = hashTable[tempValue];
+		
+		/*while that node pointer.next is not equal to null then do this */
         while(hashNode->next!=NULL)
-		{                           // scrolls down the list
+		{                          
             hashNode = hashNode->next;
         }
         hashNode->next = malloc(sizeof(hash)); 
@@ -44,9 +91,64 @@ void chainedHashInsert(long long value)
 }
 
 
+
+
+/*Resizes the silly table*/
+void resizeTable(hash *hashTable[])
+{
+	SIZE=SIZE*2;
+	hash *temp=malloc(SIZE*sizeof(hash));
+	memcpy(&table,&table,sizeof(temp));
+	int x;
+	for (x=0;x<SIZE;x++)
+		table[x]=NULL;
+	
+	
+	
+}
+
+/*Determines whether to resize or no */
+void resizeYet()
+{
+	
+	long double tableLoad=(long double)counter/(unsigned long int)SIZE;
+	if (tableLoad>=LOADFACTOR)
+	{
+		resizeTable(table);
+	}
+}
+
+
+
+
+
+
+/*Pass the value to be printed and the name of the hashtable*/
+void print(unsigned long int val,hash *hashTable[])
+{
+	
+	hash *hashMapNode = hashTable[hashNum(val)];
+	
+    while(hashMapNode!=NULL)
+	{
+        printf("%d ",hashMapNode->value);                           
+        hashMapNode = hashMapNode->next;
+    }
+
+}
+
+
 /* MAIN FUNCTION */
 int main (int argc, char const *argv[])
 {
+	chainedHashInsert(5,table); 
+	resizeTable(table);
+	resizeTable(table);
+	chainedHashInsert(6,table);
+	print(6, table);
+	printf("%d\n",SIZE);
+ 
    
 
+   
 }
